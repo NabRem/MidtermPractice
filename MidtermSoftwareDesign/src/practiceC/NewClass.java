c9669bdbafe0a506116bc3864f7df531973f5bf0
nabil <nabil@DESKTOP-Q5GQ7HI>
2020-02-17 11:52:13 -0500

    Created Master

---
+++ MidtermSoftwareDesign/src/practiceC/NewClass.java
@@ -0,0 +1,15 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package practiceC;
+
+/**
+ *
+ * @author nabil
+ * 
+ */
+public class NewClass {
+   String stupid ="Luck"; 
+}
